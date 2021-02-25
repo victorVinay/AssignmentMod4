de5c4a37b44b67a7708c226d2da838a590e7860e
victorVinay <60704591+victorVinay@users.noreply.github.com>
2021-02-25 18:41:39 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AssignmentMod4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string SrcPath = @"C:\Users\vinay singh jadon\Desktop\Promact Intership\AssignmentMod4\A.txt";
+            string DestiPath = @"C:\Users\vinay singh jadon\Desktop\Promact Intership\AssignmentMod4\B.txt";
+
+            await ReadAndWriteAsync(SrcPath, DestiPath);
+        }
+
+        private static async Task ReadAndWriteAsync(string SrcPath, string DestiPath)
+        {
+            string Content = await File.ReadAllTextAsync(SrcPath);
+            await File.WriteAllTextAsync(DestiPath, Content);
+            System.Console.WriteLine("Task Completed");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AssignmentMod4
+ 
